ae40d25dc53494a433ee9fb51ab7910e1565154e
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2022-09-14 19:14:52 -0400

    build(functions): 🚨 replace google code formatter with prettier

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -9,8 +9,8 @@ module.exports = {
     "plugin:import/errors",
     "plugin:import/warnings",
     "plugin:import/typescript",
-    "google",
     "plugin:@typescript-eslint/recommended",
+    "prettier",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
@@ -22,7 +22,6 @@ module.exports = {
   ],
   plugins: ["@typescript-eslint", "import"],
   rules: {
-    quotes: ["error", "double"],
     "import/no-unresolved": 0,
   },
 };
