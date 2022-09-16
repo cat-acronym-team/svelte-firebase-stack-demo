cc5d839be7e01afd6de55be2d235d5661925229e
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2022-09-16 15:47:29 -0400

    style: 🎨 run prettier

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,8 +4,9 @@ import { firestore } from "firebase-admin";
 import * as functions from "firebase-functions";
 
 export const setTimeStamp = functions.firestore //exporting Firebase Cloud Function setTimeStamp
-  .document('users/{docId}') //selects any document in users
-  .onCreate(async (snapshot) => { //anytime a document is created
+  .document("users/{docId}") //selects any document in users
+  .onCreate(async (snapshot) => {
+    //anytime a document is created
     const documentRef = snapshot.ref; //refer to the snapshot of the document
-    await documentRef.set({timeCreated:firestore.Timestamp.now()},{merge:true}); //create time stamp of current date/time and add it to the document
-});
+    await documentRef.set({ timeCreated: firestore.Timestamp.now() }, { merge: true }); //create time stamp of current date/time and add it to the document
+  });
