eb2227ff2659cbeb7c9d262bd1d765969e4f9098
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2022-09-16 15:31:25 -0400

    refactor(functions): 🚨 use const

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,4 @@
-//everytime a new document gets created in the users collection
+//every time a new document gets created in the users collection
 //modify the document to add timestamp to document (not override)
 import { firestore } from "firebase-admin";
 import * as functions from "firebase-functions";
@@ -6,6 +6,6 @@ import * as functions from "firebase-functions";
 export const setTimeStamp = functions.firestore //exporting Firebase Cloud Function setTimeStamp
   .document('users/{docId}') //selects any document in users
   .onCreate(async (snapshot) => { //anytime a document is created
-    let documentRef = snapshot.ref; //refer to the snapshot of the document
+    const documentRef = snapshot.ref; //refer to the snapshot of the document
     await documentRef.set({timeCreated:firestore.Timestamp.now()},{merge:true}); //create time stamp of current date/time and add it to the document
 });
