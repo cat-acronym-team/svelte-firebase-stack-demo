3b721c8937c44bd6540352eafa96a88090d1f456
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2022-09-16 16:34:34 -0400

    test(client): ✅ update homepage test

--- client/tests/test.ts
+++ client/tests/test.ts
@@ -1,6 +1,6 @@
 import { expect, test } from "@playwright/test";
 
-test("index page has expected h1", async ({ page }) => {
+test("index page has welcome message", async ({ page }) => {
   await page.goto("/");
-  expect(await page.textContent("h1")).toBe("Welcome to SvelteKit");
+  expect(await page.textContent("p")).toBe("Welcome to the Firebase Svelte Demo app brought to you by team CAT.");
 });
